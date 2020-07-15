3e34a0b723ed499591ddff20158f458bba299a84
Stanislav <stasusss07@gmail.com>
2020-07-15 03:02:31 +0300

    Updates

---
+++ precache-manifest.cdaf209c69ab9fb8af722eda0e9b317b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eb537b5654a663c581feeede1be289d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2b1bddfe35e315b8115d",
+    "url": "./static/css/main.c3c74ec3.chunk.css"
+  },
+  {
+    "revision": "4e6b1e2f8f9f5cde7ee2",
+    "url": "./static/js/2.743b3a24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.743b3a24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b1bddfe35e315b8115d",
+    "url": "./static/js/main.1b7e1901.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
